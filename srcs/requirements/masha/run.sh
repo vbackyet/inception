c932e881dc220829f5840a36a9ec33ee0878ca06
vbackyet <vbackyet@vbackyet.vbackyet>
2022-10-09 10:44:42 -0400

    masha не рабоатет

--- srcs/requirements/masha/run.sh
+++ srcs/requirements/masha/run.sh
@@ -2,12 +2,23 @@
 
 
 echo "Create Masha DB"
-# Changes the owner or group associated with a file.
-if [ ! -d "/run/mysqld" ];
-then
-	mkdir -p /run/mysqld
-fi
-chown -R 'mysql:mysql' /run/mysqld
 
-# basic install
-chown -R mysql:mysql /var/lib/mysql
+
+# mkdir -p /home/acoezard/data/database
+# mkdir -p /home/acoezard/data/www
+
+# /usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+# /usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+export MARIADB_DATABASE=mysql
+export MARIADB_USER=vbackyet
+export MARIADB_PASSWORD=123
+export MARIADB_ROOT_PASSWORD=1
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# pkill mysqld
+# /usr/bin/mysqld --user=root --datadir=/var/lib/mysql
