fadb0d38e36e7dd707605b4050f4460b0e7218d1
vbackyet <vbackyet@debian.vbackyet>
2022-10-12 12:36:14 -0400

    masha nemnogo rabotaet

--- srcs/requirements/masha/run.sh
+++ srcs/requirements/masha/run.sh
@@ -7,8 +7,8 @@ echo "Create Masha DB"
 # mkdir -p /home/acoezard/data/database
 # mkdir -p /home/acoezard/data/www
 
-# /usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-# /usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
 export MARIADB_DATABASE=mysql
 export MARIADB_USER=vbackyet
 export MARIADB_PASSWORD=123
@@ -19,6 +19,8 @@ mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED B
 mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
+echo "Clode"
+
 
 # pkill mysqld
 # /usr/bin/mysqld --user=root --datadir=/var/lib/mysql
