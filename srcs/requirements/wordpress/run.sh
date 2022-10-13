fdda22e64efbb39b9347debba50bc232970ac352
vbackyet <vbackyet@debian.vbackyet>
2022-10-13 16:39:43 -0400

    wordpress is coming

--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -2,3 +2,8 @@
 
 
 echo "Puk wordpress"
+# mkdir - r /usr/local/bin/wp
+/usr/local/bin/wp	--info
+/usr/local/bin/wp	core download --allow-root --path="/var/www/html"
+
+cp					./wp-config.php /var/www/html/wp-config.php
