09a5c5dada244acf20c56ba167a218db01a4cc7a
vbackyet <vbackyet@debian.vbackyet>
2022-10-18 20:43:42 -0400

    final

--- srcs/requirements/masha/run.sh
+++ srcs/requirements/masha/run.sh
@@ -7,13 +7,6 @@ echo "Create Masha DB"
 /usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 /usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql & sleep 2
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-# mysql -e "SHOW TABLES in ${MARIADB_DATABASE};"
-
 
 mysql -e "CREATE DATABASE  IF NOT EXISTS ${MARIADB_DATABASE};"
 mysql -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" 
